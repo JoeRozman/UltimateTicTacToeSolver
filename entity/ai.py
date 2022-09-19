0bcf739815d50d0856320fcee46abc6492a01a10
JoeRozman <jsrozman@wpi.edu>
2022-09-19 16:44:22 -0400

    Added node documentation to ai.py

--- entity/ai.py
+++ entity/ai.py
@@ -1,7 +1,11 @@
-MAX = 1000
-MIN = 1000
+import math
+
+MAX = math.inf
+MIN = -math.inf
+
 
 def minimax(depth, node, isMaxPlayer, values, alpha, beta):
+    # For the future lets think of what node is and depending on it, we will expand accordingly
     if depth == 3:
         return values[node]
 
@@ -9,7 +13,8 @@ def minimax(depth, node, isMaxPlayer, values, alpha, beta):
         V = MIN
 
         for i in range(0, 2):
-            val = minimax(depth+1, node*2+i, False, values, alpha, beta)
+            # To elaborate if node is like a listNode we would do node.child or something
+            val = minimax(depth + 1, node * 2 + i, False, values, alpha, beta)
             V = max(V, val)
             alpha = max(alpha, V)
 
@@ -18,7 +23,7 @@ def minimax(depth, node, isMaxPlayer, values, alpha, beta):
     else:
         V = MAX
         for i in range(0, 2):
-            val = minimax(depth+1, node*2+i, True, values, alpha, beta)
+            val = minimax(depth + 1, node * 2 + i, True, values, alpha, beta)
             V = min(V, val)
             beta = min(beta, V)
 
@@ -26,4 +31,3 @@ def minimax(depth, node, isMaxPlayer, values, alpha, beta):
                 break
 
     return V
-
