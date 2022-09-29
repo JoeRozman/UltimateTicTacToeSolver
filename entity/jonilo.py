10736ac064ee7dfe0e13dc86c7869b59c0c81f54
Danilo Correia <60863302+dacs30@users.noreply.github.com>
2022-09-29 18:41:11 -0400

    jonilo waits to play now

--- entity/jonilo.py
+++ entity/jonilo.py
@@ -1,4 +1,7 @@
 import math
+import os
+
+from core_gameplay import BAD_MOVE_DRAW, local_to_global
 
 MAX = math.inf
 MIN = -math.inf
@@ -6,8 +9,55 @@ MIN = -math.inf
 
 # main function
 def main():
-    # This is a temporary way of calling minimax, will call with proper parameters later
-    minimax(0, 0, False, 0, 0, 0)
+    # loop to check if there is a jonilo.go file
+    # if there is, then read the move_file and make the move
+    # if there is not, then wait for jonilo.go file to be created
+    # once jonilo.go file is created, then read the move_file and make the move
+    # then write to the move_file
+    # then delete jonilo.go file
+    # then wait for jonilo.go file to be created
+    # repeat
+    moves = []
+    exists = False
+    while True:
+        if os.path.exists("jonilo.go"):
+            exists = True
+            break
+    if exists:
+        with open("move_file", "r") as fp:
+            # Get last non-empty line from file
+            line = ""
+            for next_line in fp.readlines():
+                if next_line.isspace():
+                    break
+                else:
+                    line = next_line
+
+            # Tokenize move
+            tokens = line.split()
+
+            if len(tokens) > 0:
+                # Get board and location
+                board_num = int(tokens[1])
+                location_num = int(tokens[2])
+
+                # Convert to global coordinates
+                global_board_num, global_location_num = local_to_global(board_num, location_num)
+
+                # Add move to list
+                moves.append((global_board_num, global_location_num))
+
+                # Write to move file
+                write_to_move_file(board_num)
+
+                # Remove jonilo.go file
+                os.remove("jonilo.go")
+
+            else:
+                # Write a random move to move file
+                move_file = open("move_file", "w")
+                move_file.write("jonilo.py 2 2")
+
 
 
 def minimax(depth, board, isMaxPlayer, boardResults, alpha, beta):
@@ -52,15 +102,15 @@ def eval_function():
 
 def write_to_move_file(board):
     move_file = open("move_file", "w")
-    move_file.write(move_file_output(board))
+    move_file.write("jonilo.py 2 2")
     move_file.close()
 
 
 def move_file_output(board):
     output = ""
     agentName = "Jonilo"
-    boardNum = "board"
-    locationNum = "location"
+    boardNum = 2
+    locationNum = 2
     i = 0
 
     while i < 4:
